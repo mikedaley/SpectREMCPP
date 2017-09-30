e774285d1a46351f4e0a3d0620776025aefdf95f
Mike Daley <mike@daley-uk.com>
2017-09-30 19:46:56 +0100

    Latest with border size implemented

--- SpectREM/SpectREM/Display.fsh
+++ SpectREM/SpectREM/Display.fsh
@@ -1,16 +1,21 @@
 #version 330
 in vec2 UV;
-out vec4 color;
+out vec4 fragColor;
+
 uniform sampler2D mySampler;
+uniform float borderSize;
 
 void main()
 {
     const float w = 320;
     const float h = 256;
-    const float border = 32 - 32;
+    float border = 32 - borderSize;
+    vec3 color;
     
-    const float new_w = w - (border * 2);
-    const float new_h = h - (border * 2);
+    float new_w = w - (border * 2);
+    float new_h = h - (border * 2);
+    
+    float normalColor = 0.8153;
     
     vec2 texCoord = UV * vec2(1.0, -1.0);
     
@@ -18,77 +23,80 @@ void main()
     float v = ((texCoord.y * new_h) - border);
 
     vec2 vUv = vec2(u, v);
-    vec2 alpha = vec2(0.0); // 0.5 = Linear, 0.0 = Nearest
-    vec2 x = fract(vUv);
-    vec2 x_ = clamp(0.5 / alpha * x, 0.0, 0.5) + clamp(0.5 / alpha * (x - 1.0) + 0.5, 0.0, 0.5);
-    
-    texCoord = (floor(vUv) + x_) / vec2(w, h);
+//
+//    vec2 alpha = vec2(0.5); // 0.5 = Linear, 0.0 = Nearest
+//    vec2 x = fract(vUv);
+//    vec2 x_ = clamp(0.5 / alpha * x, 0.0, 0.5) + clamp(0.5 / alpha * (x - 1.0) + 0.5, 0.0, 0.5);
+
+    texCoord = (floor(vUv) / vec2(w, h));
 
     float c = texture( mySampler, texCoord).r * 255;
     
     if (c == 0)
     {
-        color = vec4(0, 0, 0, alpha);
+        color = vec3(0, 0, 0);
     }
     else if (c == 1)
     {
-        color = vec4(0, 0, 0.8153, alpha);
+        color = vec3(0, 0, normalColor);
     }
     else if (c == 2)
     {
-        color = vec4(0.8153, 0, 0, alpha);
+        color = vec3(normalColor, 0, 0);
     }
     else if (c == 3)
     {
-        color = vec4(0.8153, 0, 0.8153, alpha);
+        color = vec3(normalColor, 0, normalColor);
     }
     else if (c == 4)
     {
-        color = vec4(0, 0.8153, 0, alpha);
+        color = vec3(0, normalColor, 0);
     }
     else if (c == 5)
     {
-        color = vec4(0, 0.8153, 0.8153, alpha);
+        color = vec3(0, normalColor, normalColor);
     }
     else if (c == 6)
     {
-        color = vec4(0.8153, 0.8153, 0, alpha);
+        color = vec3(normalColor, normalColor, 0);
     }
     else if (c == 7)
     {
-        color = vec4(0.8153, 0.8153, 0.8153, alpha);
+        color = vec3(normalColor, normalColor, normalColor);
     }
 
     if (c == 8)
     {
-        color = vec4(0, 0, 0, alpha);
+        color = vec3(0, 0, 0);
     }
     else if (c == 9)
     {
-        color = vec4(0, 0, 1, alpha);
+        color = vec3(0, 0, 1);
     }
     else if (c == 10)
     {
-        color = vec4(1, 0, 0, alpha);
+        color = vec3(1, 0, 0);
     }
     else if (c == 11)
     {
-        color = vec4(1, 0, 1, alpha);
+        color = vec3(1, 0, 1);
     }
     else if (c == 12)
     {
-        color = vec4(0, 1, 0, alpha);
+        color = vec3(0, 1, 0);
     }
     else if (c == 13)
     {
-        color = vec4(0, 1, 1, alpha);
+        color = vec3(0, 1, 1);
     }
     else if (c == 14)
     {
-        color = vec4(1, 1, 0, alpha);
+        color = vec3(1, 1, 0);
     }
     else if (c == 15)
     {
-        color = vec4(1, 1, 1, alpha);
+        color = vec3(1, 1, 1);
     }
+    
+    fragColor = vec4(color, 1);
 }
--- SpectREM/SpectREM/OpenGLView.mm
+++ SpectREM/SpectREM/OpenGLView.mm
@@ -8,6 +8,7 @@
 
 #import "OpenGLView.h"
 #import "EmulationViewController.h"
+#import "Defaults.h"
 
 #include <iostream>
 #include <fstream>
@@ -16,7 +17,6 @@
 
 #pragma mark - Constants
 
-// Vertices of the triangle
 const GLfloat quad[] = {
     //X      Y      Z        U      V
     -1.0f,   1.0f,  0.0f,    0.0f,  1.0f,
@@ -31,17 +31,21 @@ const GLfloat quad[] = {
 
 @interface OpenGLView ()
 {
-    NSTrackingArea *_trackingArea;
-    NSWindowController *_windowController;
+    NSTrackingArea *trackingArea;
+    NSWindowController *windowController;
     
-    GLuint          _viewWidth;
-    GLuint          _viewHeight;
+    GLuint          viewWidth;
+    GLuint          viewHeight;
     
-    GLuint          _vertexBuffer;
-    GLuint          _vertexArray;
-    GLuint          _shaderProgName;
-    GLuint          _textureName;
-    GLuint          _textureID;
+    GLuint          vertexBuffer;
+    GLuint          vertexArray;
+    GLuint          shaderProgName;
+    GLuint          textureName;
+    
+    GLuint          textureID;
+    GLuint          u_borderSize;
+    
+    Defaults        *defaults;
 }
 
 @end
@@ -53,6 +57,8 @@ const GLfloat quad[] = {
 - (void) awakeFromNib
 {
     [self registerForDraggedTypes:@[NSURLPboardType]];
+    
+    defaults = [Defaults defaults];
 
     NSOpenGLPixelFormatAttribute attrs[] =
     {
@@ -80,16 +86,16 @@ const GLfloat quad[] = {
     CGLEnable([context CGLContextObj], kCGLCECrashOnRemovedFunctions);
     
     [self setPixelFormat:pf];
-    
     [self setOpenGLContext:context];
     
     NSLog(@"%s %s", glGetString(GL_RENDERER), glGetString(GL_VERSION));
     
-    _viewWidth = 320;
-    _viewHeight = 256;
+    viewWidth = 320;
+    viewHeight = 256;
     
-    // set the clear color
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
     [self loadShaders];
     [self setupTexture];
@@ -98,11 +104,12 @@ const GLfloat quad[] = {
 
 - (void) drawRect: (NSRect) theRect
 {
-    // Avoid flickering during resize by drawing
     [[self openGLContext] makeCurrentContext];
     CGLContextObj ctxObj = [[self openGLContext] CGLContextObj];
     CGLLockContext(ctxObj);
     
+    glClear(GL_COLOR_BUFFER_BIT);
+
     glDrawArrays(GL_TRIANGLES, 0, 6);
     
     CGLFlushDrawable(ctxObj);
@@ -111,10 +118,8 @@ const GLfloat quad[] = {
 
 - (void) prepareOpenGL
 {
+    [self setWantsBestResolutionOpenGLSurface:YES];
     [super prepareOpenGL];
-    
-    // Make all the OpenGL calls to setup rendering
-    //  and build the necessary rendering objects
     [[self openGLContext] makeCurrentContext];
     
     // Synchronize buffer swaps with vertical refresh rate
@@ -122,22 +127,54 @@ const GLfloat quad[] = {
     [[self openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];
 }
 
+- (void) resizeWithWidth:(GLuint)width AndHeight:(GLuint)height
+{
+    glViewport(0, 0, width, height);
+    viewWidth = width;
+    viewHeight = height;
+}
+
+- (void) reshape
+{
+    [super reshape];
+    
+    CGLLockContext([[self openGLContext] CGLContextObj]);
+    
+    NSRect viewRectPoints = [self bounds];
+    NSRect viewRectPixels = [self convertRectToBacking:viewRectPoints];
+    
+    [self resizeWithWidth:viewRectPixels.size.width
+                AndHeight:viewRectPixels.size.height];
+    
+    CGLUnlockContext([[self openGLContext] CGLContextObj]);
+}
+
+- (void)renewGState
+{
+    [[self window] disableScreenUpdatesUntilFlush];
+    [super renewGState];
+}
+
+- (BOOL)acceptsFirstResponder
+{
+    return YES;
+}
+
 #pragma mark - Renderer
 
 - (void)setupQuad
 {
-    glGenVertexArrays(1, &_vertexArray);
-    glBindVertexArray(_vertexArray);
-
-    glGenBuffers(1, &_vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(quad), quad, GL_STATIC_DRAW);
 
-    glUseProgram(_shaderProgName);
+    glUseProgram(shaderProgName);
     
     glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, _textureName);
-    glUniform1i(_textureID, 0);
+    glBindTexture(GL_TEXTURE_2D, textureName);
+    glUniform1i(textureID, 0);
     
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void *)0);
@@ -150,88 +187,43 @@ const GLfloat quad[] = {
 {
     NSString *vertexShaderPath = [[NSBundle mainBundle] pathForResource:@"Display" ofType:@"vsh"];
     NSString *fragmentShaderPath = [[NSBundle mainBundle] pathForResource:@"Display" ofType:@"fsh"];
-    _shaderProgName = LoadShaders([vertexShaderPath UTF8String], [fragmentShaderPath UTF8String]);
-    _textureID = glGetUniformLocation(_shaderProgName, "mySampler");
+    shaderProgName = LoadShaders([vertexShaderPath UTF8String], [fragmentShaderPath UTF8String]);
+    textureID = glGetUniformLocation(shaderProgName, "mySampler");
+    u_borderSize = glGetUniformLocation(shaderProgName, "borderSize");
 }
 
 - (void)setupTexture
 {
-    glGenTextures(1, &_textureName);
-    glBindTexture(GL_TEXTURE_2D, _textureName);
+    glGenTextures(1, &textureName);
+    glBindTexture(GL_TEXTURE_2D, textureName);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, 320, 256, 0, GL_RED, GL_UNSIGNED_BYTE, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 }
 
 - (void)updateTextureData:(void *)displayBuffer
 {
-    glBindTexture(GL_TEXTURE_2D, _textureName);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glBindTexture(GL_TEXTURE_2D, textureName);
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 320, 256, GL_RED, GL_UNSIGNED_BYTE, displayBuffer);
 
     [[self openGLContext] makeCurrentContext];
-    CGLContextObj ctxObj = [[self openGLContext] CGLContextObj];
-    CGLLockContext(ctxObj);
+    CGLLockContext([[self openGLContext] CGLContextObj]);
     
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
     
+    glProgramUniform1f(shaderProgName, u_borderSize, defaults.displayBorderSize);
     glDrawArrays(GL_TRIANGLES, 0, 6);
 
-    CGLFlushDrawable(ctxObj);
-    CGLUnlockContext(ctxObj);
-}
-
-- (void) resizeWithWidth:(GLuint)width AndHeight:(GLuint)height
-{
-    glViewport(0, 0, width, height);
-    _viewWidth = width;
-    _viewHeight = height;
-}
-
-- (void) reshape
-{
-    [super reshape];
-    
-    // We draw on a secondary thread through the display link. However, when
-    // resizing the view, -drawRect is called on the main thread.
-    // Add a mutex around to avoid the threads accessing the context
-    // simultaneously when resizing.
-    CGLLockContext([[self openGLContext] CGLContextObj]);
-    
-    // Get the view size in Points
-    NSRect viewRectPoints = [self bounds];
-    
-    // Points:Pixels is always 1:1 when not supporting retina resolutions
-    NSRect viewRectPixels = viewRectPoints;
-    
-    // Set the new dimensions in our renderer
-    [self resizeWithWidth:viewRectPixels.size.width
-                AndHeight:viewRectPixels.size.height];
-    
+    CGLFlushDrawable([[self openGLContext] CGLContextObj]);
     CGLUnlockContext([[self openGLContext] CGLContextObj]);
 }
 
-- (void)renewGState
-{
-    // Called whenever graphics state updated (such as window resize)
-    
-    // OpenGL rendering is not synchronous with other rendering on the OSX.
-    // Therefore, call disableScreenUpdatesUntilFlush so the window server
-    // doesn't render non-OpenGL content in the window asynchronously from
-    // OpenGL content, which could cause flickering.  (non-OpenGL content
-    // includes the title bar and drawing done by the app with other APIs)
-    [[self window] disableScreenUpdatesUntilFlush];
-    
-    [super renewGState];
-}
+#pragma mark - Load Shaders
 
-- (BOOL)acceptsFirstResponder
-{
-    return YES;
-}
-
-// Load shaders provided
 GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path){
     
     // Create the shaders
