af236d96b271b101f5b4f7c0eb13e1b4d36a6e8a
Mike <mike@71squared.com>
2017-11-04 16:19:41 +0000

    Code formatting

--- SpectREM/SpectREM/OpenGLView.mm
+++ SpectREM/SpectREM/OpenGLView.mm
@@ -509,12 +509,15 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
     // Read the Vertex Shader code from the file
     std::string VertexShaderCode;
     std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
-    if(VertexShaderStream.is_open()){
+    if(VertexShaderStream.is_open())
+    {
         std::string Line = "";
         while(getline(VertexShaderStream, Line))
             VertexShaderCode += "\n" + Line;
         VertexShaderStream.close();
-    }else{
+    }
+    else
+    {
         printf("Cannot open %s.\n", vertex_file_path);
         getchar();
         return 0;
@@ -523,7 +526,8 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
     // Read the Fragment Shader code from the file
     std::string FragmentShaderCode;
     std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
-    if(FragmentShaderStream.is_open()){
+    if(FragmentShaderStream.is_open())
+    {
         std::string Line = "";
         while(getline(FragmentShaderStream, Line))
             FragmentShaderCode += "\n" + Line;
@@ -542,7 +546,8 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
     // Check Vertex Shader
     glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
     glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    if ( InfoLogLength > 0 ){
+    if ( InfoLogLength > 0 )
+    {
         std::vector<char> VertexShaderErrorMessage(InfoLogLength+1);
         glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
         printf("%s\n", &VertexShaderErrorMessage[0]);
@@ -557,7 +562,8 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
     // Check Fragment Shader
     glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
     glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    if ( InfoLogLength > 0 ){
+    if ( InfoLogLength > 0 )
+    {
         std::vector<char> FragmentShaderErrorMessage(InfoLogLength+1);
         glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
         printf("%s\n", &FragmentShaderErrorMessage[0]);
@@ -573,7 +579,8 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
     // Check the program
     glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
     glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    if ( InfoLogLength > 0 ){
+    if ( InfoLogLength > 0 )
+    {
         std::vector<char> ProgramErrorMessage(InfoLogLength+1);
         glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
         printf("%s\n", &ProgramErrorMessage[0]);
@@ -588,7 +595,7 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
     return ProgramID;
 }
 
-#pragma mark - Drag/Drop
+#pragma mark - View Drag/Drop
 
 - (NSDragOperation)draggingEntered:(id<NSDraggingInfo>)sender
 {
@@ -602,9 +609,9 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
         if (sourceDragMask * NSDragOperationCopy)
         {
             NSURL *fileURL = [NSURL URLFromPasteboard:pBoard];
-            if ([[fileURL.pathExtension uppercaseString] isEqualToString:@"Z80"] ||
-                [[fileURL.pathExtension uppercaseString] isEqualToString:@"SNA"] ||
-                [[fileURL.pathExtension uppercaseString] isEqualToString:@"TAP"])
+            if ([[fileURL.pathExtension uppercaseString] isEqualToString:cZ80_EXTENSION] ||
+                [[fileURL.pathExtension uppercaseString] isEqualToString:cSNA_EXTENSION] ||
+                [[fileURL.pathExtension uppercaseString] isEqualToString:cTAP_EXTENSION])
             {
                 return NSDragOperationCopy;
             }
@@ -623,9 +630,9 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
     if ([[pBoard types] containsObject:NSURLPboardType])
     {
         NSURL *fileURL = [NSURL URLFromPasteboard:pBoard];
-        if ([[fileURL.pathExtension uppercaseString] isEqualToString:@"Z80"] ||
-            [[fileURL.pathExtension uppercaseString] isEqualToString:@"SNA"] ||
-            [[fileURL.pathExtension uppercaseString] isEqualToString:@"TAP"])
+        if ([[fileURL.pathExtension uppercaseString] isEqualToString:cZ80_EXTENSION] ||
+            [[fileURL.pathExtension uppercaseString] isEqualToString:cSNA_EXTENSION] ||
+            [[fileURL.pathExtension uppercaseString] isEqualToString:cTAP_EXTENSION])
         {
             EmulationViewController *emulationViewController = (EmulationViewController *)[self.window contentViewController];
             [emulationViewController loadFileWithURL:fileURL addToRecent:YES];
