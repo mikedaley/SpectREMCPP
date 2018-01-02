252e8078dd77f037bbfaca0f48e7f8ad5a4dc7ce
Mike <mike@71squared.com>
2018-01-02 18:01:48 +0000

    Started to add debugger view

---
+++ SpectREM/SpectREM/DebugViewController.h
@@ -0,0 +1,13 @@
+//
+//  DebugViewController.h
+//  SpectREM
+//
+//  Created by Mike on 02/01/2018.
+//  Copyright © 2018 71Squared Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DebugViewController : NSViewController
+
+@end
---
+++ SpectREM/SpectREM/DebugViewController.m
@@ -0,0 +1,22 @@
+//
+//  DebugViewController.m
+//  SpectREM
+//
+//  Created by Mike on 02/01/2018.
+//  Copyright © 2018 71Squared Ltd. All rights reserved.
+//
+
+#import "DebugViewController.h"
+
+@interface DebugViewController ()
+
+@end
+
+@implementation DebugViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
