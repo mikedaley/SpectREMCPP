433ee92adfac3c2baa0d376226666792107524ad
Michael Daley <michael_daley@me.com>
2019-01-08 18:47:07 +0000

    Adding a new iOS target

---
+++ SpectREM/SpectREMiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpectREMiOS
+//
+//  Created by Michael Daley on 07/01/2019.
+//  Copyright © 2019 71Squared Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
